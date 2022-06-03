a70e380467dfedd5d4a26c71235a4fc66bc65237
An Nguyen <103458192+ElNopalito@users.noreply.github.com>
2022-06-03 17:46:18 -0400

    Update server.js

--- Nodejs/pokemon_app/server.js
+++ Nodejs/pokemon_app/server.js
@@ -6,7 +6,8 @@ const pokemonData = require('./Models/pokemon')
 const app = express()
 //! This is the port our local host will run on. If something is not working correctly, just set it 3001,3002,etcetc until it works.
 const PORT = 3000
-
+app.set('view engine', 'ejs')
+app.set('views', './Views')
 
 //!This is setting up our home server. Usually it will start with app.get(a file route using "/", (req,res) then a function =>{
     //!Within the function will have a res.send('insert string or data')
@@ -14,9 +15,17 @@ const PORT = 3000
 app.get('/', (req,res) => {
     res.send('Welcome to the Pokemon App')
 })
-//! We are setting up our Pokemon server. We can have as many servers as needed. We are res.send the array that was on pokemon.js. 
-app.get('/pokemon', (req,res) => {
-    res.send(pokemonData)
+// //! We are setting up our Pokemon server. We can have as many servers as needed. We are res.send the array that was on pokemon.js. 
+// app.get('/pokemon', (req,res) => {
+//     res.send(pokemonData)
+// })
+//! We are making it so that our Pokemon server will render the index.ejs file and the pokemonData var, which is the data of the pokemon.js file.
+app.get('/pokemon',(req,res) => {
+    res.render('index', {
+        pageTitle: 'Pokemon',
+        pageHeader: 'See all the Pokemon!',
+        pokemonData: pokemonData,
+    })
 })
 //! After app.get will be app.listen. Basically it will listen a act as a return function. So when app.get function is trriggered, the app.listen function will trigger too. Usually it will have console.log(`Server is running on ${PORT})
 app.listen(PORT, () => {
