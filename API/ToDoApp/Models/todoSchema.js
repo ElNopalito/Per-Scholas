47d504bc330717354869e6754ba2cf8d97b479f4
An Nguyen <103458192+ElNopalito@users.noreply.github.com>
2022-06-13 11:30:26 -0700

    Update todoSchema.js

--- API/ToDoApp/Models/todoSchema.js
+++ API/ToDoApp/Models/todoSchema.js
@@ -1,5 +1,7 @@
+//!---------------------------------------------Using Mongoose to configure our Schema---------------------------------------------------
 const mongoose = require('mongoose')
 
+//! Mongoose will look at your Schema and document what is required. If you try to input in Postman without all required details, it will come up as an error.
 const todoSchema = mongoose.Schema({
     title: {
         type : String,
@@ -12,6 +14,7 @@ const todoSchema = mongoose.Schema({
     },
 
     completed: {
+        type: Boolean,
         default: false
     },
 
