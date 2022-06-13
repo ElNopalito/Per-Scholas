ddb0a461ee763081f677b65328e224aeb2b1fdbf
An Nguyen <103458192+ElNopalito@users.noreply.github.com>
2022-06-13 11:32:03 -0700

    Update todosRouter.js

--- API/ToDoApp/route/todosRouter.js
+++ API/ToDoApp/route/todosRouter.js
@@ -1,19 +1,25 @@
-const express = require('express')
-const todoModel = require('../Models/todoSchema')
-//! Create router
-const router = express.Router()
+//!-----------------------------------------------------------Creating Routes for todos------------------------------------------------
+const express = require('express');
 
+//! The todoSchema is the list of require parameters in the Schema file. It is now todoModel
+const todoModel = require('../Models/todoSchema');
 
+//! Create router using express.router()
+const router = express.Router();
+
+//! Getting all todos from todoSchema
 router.get('/', async (req, res) => {
+
     try {
         const todos = await todoModel.find()
         res.status(200).json(todos)
     } catch (error) {
         console.log(error)
     }
- })
+ });
 
-
+//! This is the Post. We first are creating a object called todoData and assigning it the value of req.body. req.body allows us to request the body of the Schema and sending it to server.js
+//! In our try, we are awaiting for the schema to create a new todoData and is assigned to var todo
 router.post('/', async (req,res) =>{
     const todoData = req.body //! getting body data using req method
 
@@ -25,9 +31,9 @@ router.post('/', async (req,res) =>{
         console.error(error);
         res.status(400).json('HELP ME')
     }
-})
+});
 
-//! Get todo by ID
+//! Get todo by specific ID
 router.get('/:id', async (req,res)=>{
     const id = req.params.id
     try {
@@ -35,6 +41,33 @@ router.get('/:id', async (req,res)=>{
         res.status(200).json(todo)
     } catch (error) {
         console.error(error);
+        res.status(400).json({
+            msg: 'ID not found!'
+        })
+    }
+});
+
+//! Updating new Todos by ID
+router.put('/:id', async (req,res) => {
+    const id = req.params.id
+    const newtodoData = req.body
+    try {
+        const todo = await todoModel.findByIdAndUpdate(id, newtodoData)  //! Going to find ID and update
+        res.status().json(todo)
+    } catch(error){
+        console.log(error);
+    }
+});
+
+//! Deleting a Todo by ID
+router.delete('/:id', async (req,res) => {
+    const od = req.params.id
+
+    try {
+        const todo = await todoModel.findByIdAndDelete
+        res.status(200).json({msg: 'Todo was deleted'})
+    } catch (error) {
+        console.log(error);
     }
 })
 module.exports = router
