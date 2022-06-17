48c706694086e3291cff4fdbfc10e22628d72805
An Nguyen <103458192+ElNopalito@users.noreply.github.com>
2022-06-16 21:36:05 -0700

    Update server.js

--- API/ToDoApp/server.js
+++ API/ToDoApp/server.js
@@ -1,24 +1,31 @@
 const express = require('express');
-const { default: helmet } = require('helmet');
-require('dotenv').config()// init dotenv
+//! Hides browser info
+const helmet = require('helmet');
+//! Checks activity
+const morgan = require('morgan');
+//! Hides passwords
+const bcrypt = require('bcrypt');
+require('dotenv').config();// init dotenv
 
 //! We are imporing mongoConfig which is where Mongoose is connected.
-const mongoConfig = require('./config/mongoConfig')
+const mongoConfig = require('./config/mongoConfig');
 
 //! Routers
 const todosRouter = require('./route/todosRouter');
-const userRouter = require('./route/userRouter')
+const userRouter = require('./route/userRouter');
 
 const app = express();
 const PORT = 3000;
 
-app.use(express.json())
+app.use(express.json());
 //! Security
-app.use(helmet())
+app.use(helmet());
+//! Monitoring
+app.use(morgan('dev'));
 
 //! Importing Router
-app.use('/todos', todosRouter)
-app.use('/users', userRouter)
+app.use('/todos', todosRouter);
+app.use('/users', userRouter);
 //!-------------------------------------MAIN ROUTE--------------------------------------------------------------------------------------------------------------------
 app.get('/', (req,res) => {
     res.status(200).json({message: 'welcome to API'}) //! res.status allows us to send custom 200 code.
@@ -27,7 +34,7 @@ app.get('/', (req,res) => {
 
 
 
-//!----------------------------------Listening---------------------------------------------------------------------------------------------------------------------
+//!--------------------------------------Listening---------------------------------------------------------------------------------------------------------------------
 app.listen(PORT, () =>{
     console.log(`Server is running on ${PORT}`);
     mongoConfig();
