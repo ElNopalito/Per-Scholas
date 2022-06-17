ecbb50e23238d29054a8569ee1aff9001347bb45
An Nguyen <103458192+ElNopalito@users.noreply.github.com>
2022-06-16 23:35:43 -0700

    Update server.js

--- API/ToDoApp/server.js
+++ API/ToDoApp/server.js
@@ -5,6 +5,7 @@ const helmet = require('helmet');
 const morgan = require('morgan');
 //! Hides passwords
 const bcrypt = require('bcrypt');
+
 require('dotenv').config();// init dotenv
 
 //! We are imporing mongoConfig which is where Mongoose is connected.
@@ -13,6 +14,7 @@ const mongoConfig = require('./config/mongoConfig');
 //! Routers
 const todosRouter = require('./route/todosRouter');
 const userRouter = require('./route/userRouter');
+const authRouter = require('./route/authRouter')
 
 const app = express();
 const PORT = 3000;
@@ -26,6 +28,7 @@ app.use(morgan('dev'));
 //! Importing Router
 app.use('/todos', todosRouter);
 app.use('/users', userRouter);
+app.use('/auth', authRouter)
 //!-------------------------------------MAIN ROUTE--------------------------------------------------------------------------------------------------------------------
 app.get('/', (req,res) => {
     res.status(200).json({message: 'welcome to API'}) //! res.status allows us to send custom 200 code.
