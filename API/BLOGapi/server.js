4dad284fb284b6cc2926d206b2c3832085b117b9
An Nguyen <103458192+ElNopalito@users.noreply.github.com>
2022-06-16 21:38:46 -0700

    Update server.js

--- API/BLOGapi/server.js
+++ API/BLOGapi/server.js
@@ -1,15 +1,27 @@
-const express = require('express')
-const app = express()
-require('dotenv').config()
-const PORT = 3000
+const express = require('express');
+const app = express();
+require('dotenv').config();
+const PORT = 3000;
+const morgan = require('morgan');
+const helmet = require('helmet');
 
-const mongoConfig = require('./Config/mongoConfig')
+const mongoConfig = require('./Config/mongoConfig');
 
+const userModel = require('./Models/userSchema');
+const blogModel = require('./Models/blogSchema');
+
+const userRouter = require('./Route/userRouter');
+const blogRouter = require('./Route/blogRouter');
+
+app.use(express.json());
+app.use('Blog', blogRouter);
+app.use('/User', userRouter);
+//!----------------------------------------MAIN SERVER------------------------------------- 
 app.get('/blog', (req,res) => {
     res.status.json({message: 'Welcome to API'})
 })
-
-app.listen(PORT, () =>{
+//!----------------------------------------APP LISTEN---------------------------------------
+app.listen(PORT, () => { 
     console.log(`Server is running on ${PORT}`);
     mongoConfig();
 })
