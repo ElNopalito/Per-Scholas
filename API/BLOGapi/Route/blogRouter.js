7cb02f1d483879ac523256181a7bc1a38c4a04cb
An Nguyen <103458192+ElNopalito@users.noreply.github.com>
2022-06-16 21:38:07 -0700

    Update blogRouter.js

--- API/BLOGapi/Route/blogRouter.js
+++ API/BLOGapi/Route/blogRouter.js
@@ -1,10 +1,10 @@
-const express = require('express')
+const express = require('express');
 
 const router = express.Router();
 
-const blogModel = require('../Models/blogSchema')
+const blogModel = require('../Models/blogSchema');
 
-route.post('/', async (req,res) => {
+router.post('/', async (req,res) => {
     const blogData = req.body
 
     try {
@@ -14,5 +14,40 @@ route.post('/', async (req,res) => {
         console.log(error);
         res.status(400).json('Try again')
     }
-})
+});
 
+router.get('/:blog', async (req,res) => { 
+    const blog = req.params.id
+    try {
+        const todo = await todoModel.findById(blog)
+        res.status(200).json(blog)
+    } catch (error) {
+        console.error(error);
+        res.status(400).json({msg: 'ID not found!'})
+    }
+});
+
+router.put('/:blog', async (req,res) => {
+    const blog = req.params.id
+    const newBlogData = req.body
+    try {
+        const todo = await todoModel.findByIdAndUpdate(blog, newBlogData)  //! Going to find ID and update
+        res.status(202).json(blog)
+    } catch(error){
+        console.log(error);
+        res.status(400).json({msg: "Can't update blog"})
+    }
+});
+
+router.delete('/:blog', async (req,res) => {
+    const blog = req.params.id
+
+    try {
+        const blog = await blogModel.findByIdAndDelete(blog)
+        res.status(200).json({msg: 'Blog was deleted'})
+    } catch (error) {
+        console.log(error);
+        res.status(400).json({msg:'Unable to delete'})
+    }
+});
+module.exports = router
