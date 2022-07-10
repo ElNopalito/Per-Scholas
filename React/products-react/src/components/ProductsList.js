eae7650aceeec487389e0b37c7773e7d98fc7084
An Nguyen <103458192+ElNopalito@users.noreply.github.com>
2022-07-10 12:51:15 -0700

    Update ProductsList.js

--- React/products-react/src/components/ProductsList.js
+++ React/products-react/src/components/ProductsList.js
@@ -8,34 +8,31 @@ import ProductItem from "./ProductItem";
 //     return(
 //         <div>
 //             <h2>Products</h2>
-            //!To call up a specfic opbject in array, we use syntax below.
+//             !To call up a specfic opbject in array, we use syntax below.
 //             <h3> {props.products[0].name} </h3>
 //         </div>
 //     )
 // }
 
-//! Naming all products in array to show on page
+// ! Naming all products in array to show on page
 const ProductsList = (props) => {
-    console.log(props.products);
     return(
         <div>
             <h2>Products</h2>
-            
-            {/*props.products.map((product, idx) => <ProductItem item={product} key={idx} />)*/}  
+              {/*props.products.map((product, idx) => <ProductItem product={item} key={idx} />)*/}  
              
            {/*Using Tenerary operator(if/else) where if the item is inStock(check productData). If not, it will render product.name is out of stock in an h3*/}
            {/* {
-//                 props.products.map( (item, idx) => item.inStock ? (
+//                 props.products.map( (product, idx) => item.inStock ? (
 //                      <ProductItem product={item} key={idx}/>
 //                      ) : 
 //                      ( 
-//                      <h3> key={idx} {item.name} is out of stock</h3>
+//                      <h3> key={idx} {product.name} is out of stock</h3>
 //                      )
 //                      )} */}
-            {/* Double End Operator. On either side of the operator, both statements must be true. If one statement is not, the item will not show on list*/ }
             {
                 props.products.map(
-                     (item, idx ) => item.inStock  && <ProductItem product={item} key={idx} addtoCart={props.addtoCart} />)
+                     (item, idx ) => item.inStock  && <ProductItem product={item} key={idx} addToCart={props.addToCart} />)
             }
         </div>
     )
