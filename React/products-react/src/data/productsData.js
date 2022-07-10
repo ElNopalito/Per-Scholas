c8dbe215cca2b3e1accd27b3f7c717a521af252c
An Nguyen <103458192+ElNopalito@users.noreply.github.com>
2022-07-10 10:16:33 -0700

    Update productsData.js

--- React/products-react/src/data/productsData.js
+++ React/products-react/src/data/productsData.js
@@ -1,104 +1,131 @@
 const products = [
-    {
-      "name": "allen wrench",
-      "price": 2.99,
-      "description": "handy tool"
-    },
-    {
-      "name": "cornucopia",
-      "price": 5.99,
-      "description": "festive holiday decoration"
-    },
-    {
-      "name": "banana",
-      "price": 0.99,
-      "description": "full of potassium"
-    },
-    {
-      "name": "guillotine (cigar)",
-      "price": 10.59,
-      "description": "behead your stub"
-    },
-    {
-      "name": "jack-o-lantern",
-      "price": 3.99,
-      "description": "spooky seasonal fun"
-    },
-    {
-      "name": "doggie treat (box)",
-      "price": 5.99,
-      "description": "fido loves 'em"
-    },
-    {
-      "name": "egg separator",
-      "price": 3.99,
-      "description": "it separates yolks from whites"
-    },
-    {
-      "name": "LED lightbulb",
-      "price": 6.55,
-      "description": "It's super-efficient!"
-    },
-    {
-      "name": "owl pellets",
-      "price": 3.99,
-      "description": "Don't ask what they used to be."
-    },
-    {
-      "name": "flag",
-      "price": 5.99,
-      "description": "catches the breeze"
-    },
-    {
-      "name": "hair brush",
-      "price": 6.99,
-      "description": "fine boar bristles"
-    },
-    {
-      "name": "iridium (one gram)",
-      "price": 19.36,
-      "description": "corrosion-resistant metal"
-    },
-    {
-      "name": "quark",
-      "price": 0.01,
-      "description": "Very small"
-    },
-    {
-      "name": "turtleneck",
-      "price": 19.99,
-      "description": "available in black and slightly-darker black"
-    },
-    {
-      "name": "kaleidoscope",
-      "price": 8.25,
-      "description": "tube with moving plastic pieces inside"
-    },
-    {
-      "name": "mitt (leather)",
-      "price": 15,
-      "description": "regulation sized"
-    },
-    {
-      "name": "nothing",
-      "price": 10,
-      "description": "Hey, if you pay us, we won't ask any questions."
-    },
-    {
-      "name": "violin",
-      "price": 2000,
-      "description": "Talk about a JS fiddle..."
-    },
-    {
-      "name": "yoyo",
-      "price": 1,
-      "description": "We had to pull some strings to get this one in."
-    },
-    {
-      "name": "pincushion",
-      "price": 1.99,
-      "description": "You'll get 'stuck' on it"
-    },
-  ]
+  {
+    "name": "spoon",
+    "price": 22.99,
+    "description": "handy tool",
+    "inStock": true,
+  },
+  {
+    "name": "allen wrench",
+    "price": 2.99,
+    "description": "handy tool",
+    "inStock": true,
+  },
+  {
+    "name": "cornucopia",
+    "price": 5.99,
+    "description": "festive holiday decoration",
+    "inStock": true,
+  },
+  {
+    "name": "banana",
+    "price": 0.99,
+    "description": "full of potassium",
+    "inStock": true,
+  },
+  {
+    "name": "guillotine (cigar)",
+    "price": 10.59,
+    "description": "behead your stub",
+    "inStock": true,
+  },
+  {
+    "name": "jack-o-lantern",
+    "price": 3.99,
+    "description": "spooky seasonal fun",
+    "inStock": true,
+  },
+  {
+    "name": "doggie treat (box)",
+    "price": 5.99,
+    "description": "fido loves 'em",
+    "inStock": true,
+  },
+  {
+    "name": "egg separator",
+    "price": 3.99,
+    "description": "it separates yolks from whites",
+    "inStock": true,
+  },
+  {
+    "name": "LED lightbulb",
+    "price": 6.55,
+    "description": "It's super-efficient!",
+    "inStock": true,
+  },
+  {
+    "name": "owl pellets",
+    "price": 3.99,
+    "description": "Don't ask what they used to be.",
+    "inStock": true,
+  },
+  {
+    "name": "flag",
+    "price": 5.99,
+    "description": "catches the breeze",
+    "inStock": true,
+  },
+  {
+    "name": "hair brush",
+    "price": 6.99,
+    "description": "fine boar bristles",
+    "inStock": true,
+  },
+  {
+    "name": "iridium (one gram)",
+    "price": 19.36,
+    "description": "corrosion-resistant metal",
+    "inStock": true,
+  },
+  {
+    "name": "quark",
+    "price": 0.01,
+    "description": "Very small",
+    "inStock": true,
+  },
+  {
+    "name": "turtleneck",
+    "price": 19.99,
+    "description": "available in black and slightly-darker black",
+    "inStock": true,
+  },
+  {
+    "name": "kaleidoscope",
+    "price": 8.25,
+    "description": "tube with moving plastic pieces inside",
+    "inStock": true,
+  },
+  {
+    "name": "mitt (leather)",
+    "price": 15,
+    "description": "regulation sized",
+    "inStock": true,
+  },
+  {
+    "name": "nothing",
+    "price": 10,
+    "description": "Hey, if you pay us, we won't ask any questions.",
+    "inStock": true,
+  },
+  {
+    "name": "violin",
+    "price": 2000,
+    "description": "Talk about a JS fiddle...",
+    "inStock": false,
+  },
+  {
+    "name": "yoyo",
+    "price": 1,
+    "description": "We had to pull some strings to get this one in.",
+    "inStock": false,
+  },
+  {
+    "name": "pincushion",
+    "price": 1.99,
+    "description": "You'll get 'stuck' on it",
+    "inStock": false,
+  },
+];
 
-  export default products
+export default products; 
+
