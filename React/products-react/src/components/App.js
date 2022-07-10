5ecf4305dde43b099c685f411ab2cc89a22fb1ea
An Nguyen <103458192+ElNopalito@users.noreply.github.com>
2022-07-10 12:50:34 -0700

    Update App.js

--- React/products-react/src/components/App.js
+++ React/products-react/src/components/App.js
@@ -4,6 +4,9 @@ import Navbar from './Navbar';
 import ProductsList from './ProductsList';
 import productsData from '../data/productsData';
 import Form from './Form';
+import ShoppingCart from './ShoppingCart';
+
+
 //!-----------------------------------------------------------------Main App Component--------------------------------------------------------------
 class App extends Component { 
 //! A state is a built-in React object that is used to contain data or information about the component. We would presumably use state because different Components may serve many different functions.
@@ -13,6 +16,7 @@ class App extends Component {
     //! In ES6 syntax, if the key and value are the same, you can just input the key without having to input value 
     //! A Component can only have 1 state but can have multiple information in the state. 
     productsData: productsData,
+    shoppingCartItems: [],
     productName: '' ,
     productPrice: 0,
     productDescription: ''
@@ -26,7 +30,6 @@ class App extends Component {
     console.log(event.target.value);
     this.setState({ [event.target.id] : event.target.value})
   }
-//!-----------------------------------------------------------------Where State will append information to current list/array---------------------------------------
   //! This method will allow the state to refresh the page with the values we inputed in the form in the console.
   handleSubmit = (event) => {
     event.preventDefault();
@@ -38,6 +41,7 @@ class App extends Component {
       inStock: true
     }
     console.log(newProduct);
+//!-----------------------------------------------------------------Where State will append information to current list/array---------------------------------------
     //! We are updating the array by creating another array to add to productsData array. When we input information, it will create another array to add to productsData
     this.setState({
     productsData: [...this.state.productsData, newProduct],
@@ -46,12 +50,18 @@ class App extends Component {
     productDescription: ''  })
   }
 
-
+//!----------------------------------------------------------------Adding Items to Shopping Cart------------------------------------------------------------
+addToCart = (item) => {
+  console.log(item);
+  // this.setState({shoppingCartItems: [item, ...this.stateCartItems]})
+}
 //!----------------------------------------------------------------What shows in browser--------------------------------------------------------------------
   render(){
     return (
       <div className="App">
         <Navbar />
+        {/*Any time there is a object in the state, we will always use this.state because it is in the state and it is a object that will be manipulated and re-renders*/}
+        <ShoppingCart shoppingCartItems = {this.state.shoppingCartItems}/>
           {/* This form was originally hardcoded in the App component. However, we created a Form component and moved the information there. But because handleChange and handleSubmit are not  global methods, they would not be in the Form.js file. To render them, we manipulate it here in the Form component within the App component using state.*/}
        <Form 
         handleSubmit = {this.handleSubmit}
@@ -61,7 +71,7 @@ class App extends Component {
         productDescription = {this.state.productDescription} 
         />
           {/* We are importing productsData and passing it to the App component with the ProductsList component. To do so, we must create a key name and in brackets{}, we pass in productsData*/}
-        <ProductsList products={this.state.productsData} />
+        <ProductsList products={this.state.productsData} addToCart={this.addToCart} />
       </div>
     );
   }
