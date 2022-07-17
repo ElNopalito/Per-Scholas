9da54f97ba38bab751965e59fc46594cdc33cb02
An Nguyen <103458192+ElNopalito@users.noreply.github.com>
2022-07-17 10:29:43 -0700

    Update mongoConfig.js

--- API/BLOGapi/Config/mongoConfig.js
+++ API/BLOGapi/Config/mongoConfig.js
@@ -1,5 +1,5 @@
 //!----------------------------------------------------------Connecting Mongoose to Module--------------------------------------
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 //! This function is a try/catch. Either the try works or it will catch an error. In the try, the function is awaiting to make a connection to MongoDB with the URI stated in out .env file. After it is sucessful, it will console.log a message
 //! If it fails, it will catch an error and console.log that error
@@ -9,6 +9,6 @@ module.exports = async () => {
         mongoose.connection //! checking for connection
         console.log('MongoDB Connected'); 
     } catch (error) {
-        console.error(error);
+        console.log('Something is wrong with ENV');
     }
 }
