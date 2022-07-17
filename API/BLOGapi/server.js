5f3714f3970efed7a8a3f7606b28fd5c88bca9d4
An Nguyen <103458192+ElNopalito@users.noreply.github.com>
2022-07-17 10:27:52 -0700

    Update server.js

--- API/BLOGapi/server.js
+++ API/BLOGapi/server.js
@@ -2,27 +2,33 @@ const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
 const bcrypt = require('bcrypt')
+//!---------------------------------------------MongoDB--------------------------------------
 require('dotenv').config();
+//!---------------------------------------------Hero Roku------------------------------------
+const PORT = process.env.PORT || 3000;
 
-const PORT = 3000;
-
+//!-------------------------------------------Modules Config---------------------------------
 const mongoConfig = require('./Config/mongoConfig');
 
-const userModel = require('./Models/userSchema');
-const blogModel = require('./Models/blogSchema');
-
+//!-----------------------------------------Importing Routers--------------------------------
+const authRouter = require('./Route/authRouter')
 const userRouter = require('./Route/userRouter');
 const blogRouter = require('./Route/blogRouter');
 
 const app = express();
 app.use(express.json());
+app.use(helmet());
+app.use(morgan('dev'));
+
+//!--------------------------------------------Router Config-----------------------------------
 app.use('/Blog', blogRouter);
 app.use('/User', userRouter);
-//!----------------------------------------MAIN SERVER------------------------------------- 
+app.use('/Auth', authRouter)
+//!--------------------------------------------MAIN SERVER-------------------------------------- 
 app.get('/', (req,res) => {
     res.status(200).json({message: 'Welcome to API'})
 })
-//!----------------------------------------APP LISTEN---------------------------------------
+//!---------------------------------------------APP LISTEN---------------------------------------
 app.listen(PORT, () => { 
     console.log(`Server is running on ${PORT}`);
     mongoConfig();
