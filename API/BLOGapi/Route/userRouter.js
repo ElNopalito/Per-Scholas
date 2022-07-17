f6a8adb58978877bc563b3ae583834046a00ef97
An Nguyen <103458192+ElNopalito@users.noreply.github.com>
2022-07-17 10:28:14 -0700

    Update userRouter.js

--- API/BLOGapi/Route/userRouter.js
+++ API/BLOGapi/Route/userRouter.js
@@ -1,20 +1,61 @@
 const express = require('express');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const {check, validationResult} = require('express-validator');
 
 const router = express.Router();
 
 const userModel = require('../Models/userSchema');
 
-router.post('/', async (req,res) => {
+//!------------------------------------------POST User-----------------------------------------
+router.post('/', [
+    check('username', "Username is required!").notEmpty(),
+    check("email", "Please use valid email").isEmail(),
+    check("password", 'Please enter a password').notEmpty(),
+    check('password', 'Please enter a password with 6 or more characters').isLength({min:6}),
+  
+] ,async (req,res) => {
     const userData = req.body
-
+    const error = validationResult(req)
+  
+    if (!error.isEmpty()) {                 
+       return res.json(error.array())                   
+    };
+    
     try {
+
+         const userExist = await userModel.findOne({email: userData.email});
+         
+         if (userExist){
+            return res.json({msg: 'User already exist'})
+         }
+
+        //! salt rounds
+        const saltRounds = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(userData.password, saltRounds)
+        console.log(hashedPassword);
+        userData.password = hashedPassword
         const user = await userModel.create(userData)
-        res.status(201).json(user)
+
+    
+         const payLoad = {
+            id: user._id,
+            email: user.email
+         }
+
+         const SECRET_KEY= 'INSERT RANDOM HASH'
+
+         const TOKEN = jwt.sign(payLoad, SECRET_KEY)
+
+        res.status(201).json({
+            user: user,
+            token: TOKEN
+        })
     } catch (error) {
         console.log(error);
-        res.status(400).json("Nah, didn't work")
+        res.status(400).json('TRY AGAIN')
     }
-});
+})
 
 router.get('/', async (req,res) => {
     try {
@@ -24,30 +65,7 @@ router.get('/', async (req,res) => {
         console.log(error);
         res.status(400).json('TRY AGAIN')
     }
-});
-
-router.get('/:id', async (req,res) => {
-    const id = req.params.id
-    try {
-        const user = await userModel.findById(id)
-        res.status(200).json(user)
-    } catch (error) {
-        console.log(error);
-        res.status(400).json('TRY AGAIN')
-    }
-});
-
-router.put('/:id', async (req,res) => {
-    const id = req.params.id
-    const newUserData = req.body
-
-    try {
-        const user = await userModel.findById(id)
-        res.status(200).json(user)
-    } catch (error) {
-        console.log(error);
-        res.status(400).json({msg: 'ID not found!'})    }
-});
+})
 
 router.delete('/:id', async (req,res) => {
     const id = req.params.id
@@ -57,7 +75,7 @@ router.delete('/:id', async (req,res) => {
         res.status(200).json({msg: 'User was deleted'})
     } catch (error) {
         console.log(error);
-        res.status(400).json({msg: 'Unable to delete'})
+        res.status(400).json('Unable to delete user!')
     }
 });
 
